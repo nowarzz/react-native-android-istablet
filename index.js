0826db0f1275454ca16b2e9f3e9f2e59fbdcbb50
Willy Martin <lucky.nowarzz@gmail.com>
2022-07-06 14:39:07 +0700

    update ratio for phone

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-import { PixelRatio, Dimensions } from 'react-native'
+import { PixelRatio, Dimensions } from 'react-native';
+
+const gcd = (a, b) => {
+  if (b === 0) {
+    return a;
+  }
+  return gcd(b, a % b);
+};
 
 const isTablet = () => {
   let pixelDensity = PixelRatio.get();
@@ -6,6 +13,12 @@ const isTablet = () => {
   let height = Dimensions.get("window").height;
   const adjustedWidth = width * pixelDensity;
   const adjustedHeight = height * pixelDensity;
+  const ratioDivider = gcd(width,height);
+  const wRatio = width / ratioDivider;
+  const hRatio = height / ratioDivider;
+  if (wRatio === 9 && hRatio === 20) {
+    return false;
+  }
   if (pixelDensity < 2 && (adjustedWidth >= 1000 || adjustedHeight >= 1000)) {
     return true;
   } else
